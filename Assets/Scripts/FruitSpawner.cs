a74db3bb756e7d75bd0baf08a81c04aba1f5ae64
PTRL481\Anusha.Medichelmi <anusha.m@xcubelabs.com>
2022-05-19 13:00:26 +0530

    UI part is done

--- Assets/Scripts/FruitSpawner.cs
+++ Assets/Scripts/FruitSpawner.cs
@@ -5,21 +5,23 @@ using UnityEngine;
 public class FruitSpawner : MonoBehaviour
 {
     [SerializeField]
-    private GameObject appleReference;
+    private GameObject[] Fruits;
     private Vector3 throwForce;
 
     void Start()
     {
         throwForce = new Vector3(0, 10, 0);
-        InvokeRepeating("SpawnFruit", 0.5f, 6);
+        InvokeRepeating("SpawnFruit", 0.5f, 5);
     }
 
     void SpawnFruit()
     {
-        for (byte i = 0; i < 4; i++)
+        //  for (int i = 0; i < 5; i++)
+        foreach (GameObject item in Fruits)
         {
-            GameObject fruit = Instantiate(appleReference, new Vector3(Random.Range(-6.6f, 6.6f), Random.Range(-8.0f, -1.0f), 0.0f), Quaternion.identity) as GameObject;
+            GameObject fruit = Instantiate(item, new Vector3(Random.Range(-6.6f, 6.6f), Random.Range(-8.0f, -1.0f), 0.0f), Quaternion.identity) as GameObject;
             fruit.GetComponent<Rigidbody2D>().AddForce(throwForce, ForceMode2D.Impulse);
+
         }
     }
 }
