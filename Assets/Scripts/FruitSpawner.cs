d15f24f9e7929cf56ec9559711b59071c811f0dc
PTRL481\Anusha.Medichelmi <anusha.m@xcubelabs.com>
2022-05-19 13:50:58 +0530

    If time exceeds game ends

--- Assets/Scripts/FruitSpawner.cs
+++ Assets/Scripts/FruitSpawner.cs
@@ -6,22 +6,27 @@ public class FruitSpawner : MonoBehaviour
 {
     [SerializeField]
     private GameObject[] Fruits;
-    private Vector3 throwForce;
+    public float throwForce;
+    public float time;
 
     void Start()
     {
-        throwForce = new Vector3(0, 10, 0);
-        InvokeRepeating("SpawnFruit", 0.5f, 5);
+
+
     }
 
-    void SpawnFruit()
+    void Update()
     {
-        //  for (int i = 0; i < 5; i++)
-        foreach (GameObject item in Fruits)
-        {
-            GameObject fruit = Instantiate(item, new Vector3(Random.Range(-6.6f, 6.6f), Random.Range(-8.0f, -1.0f), 0.0f), Quaternion.identity) as GameObject;
-            fruit.GetComponent<Rigidbody2D>().AddForce(throwForce, ForceMode2D.Impulse);
 
-        }
+
+        time = time + Time.deltaTime;
+        if (time > 5f)
+            foreach (GameObject item in Fruits)
+            {
+                GameObject fruit = Instantiate(item, new Vector3(Random.Range(-6.6f, 6.6f), Random.Range(-8.0f, -1.0f), 0.0f), Quaternion.identity) as GameObject;
+                fruit.GetComponent<Rigidbody2D>().AddForce(Vector2.up * throwForce, ForceMode2D.Impulse);
+                time = 0f;
+                
+            }
     }
 }
